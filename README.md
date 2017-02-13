d003bb79448a80619084cd9dd784511c260dadc5
dongwenxiao <cs_victor@126.com>
2017-02-13 14:53:24 +0800

    完成基础版上传功能

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # sp-upload
 Upload file module for sp-base
+
+## 逻辑
+
+上传文件到指定文件夹，数据表记录上传的文件。
+
+## 接口
+
+```
+// 路由
+
+/upload/file
+/upload/image
+/upload/audio
+/upload/video
+/upload/xxx
+```
--- src/router.js
+++ src/router.js
@@ -1,5 +1,59 @@
 import Router from 'koa-router'
 
+const multer = require('koa-multer')
+const moment = require('moment')
+const mkdirp = require('mkdirp')
+const fs = require('fs')
+const path = require('path')
+const md5 = require('md5')
+
+const FILE_SAVE_PATH = process.cwd() + '/src/server/public/upload'
+const FILE_BASE_URL = 'http://localhost:3000/upload/'
+
+
+
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+
+        // console.log(req)
+        // console.log(req.body.tag)
+
+        let dir = FILE_SAVE_PATH
+
+        if (!fs.existsSync(dir)) {
+            mkdirp(dir, function(err) {
+                if (err) console.error(err)
+                else cb(null, dir)
+            })
+
+        } else {
+            cb(null, dir)
+        }
+    },
+
+    filename: function(req, file, cb) {
+
+        const ext = path.extname(file.originalname)
+        const filename = md5(moment().format('YYYY.MM.DD.HH.mm.ss.SSS') + Math.random()) + ext
+
+        req.handleResult = {
+            url: FILE_BASE_URL + filename,
+            originalFilename: file.originalname,
+            filename: filename,
+            encoding: file.encoding,
+            mimeType: file.mimetype
+        }
+
+        // console.log(req.handleResult)
+
+        cb(null, filename)
+
+
+    }
+})
+const upload = multer({ storage: storage })
+
+
 /**
  * 创建upload相关路由
  *
@@ -11,10 +65,10 @@ export default function createRouter() {
     const router = new Router()
 
     return router
-        .get('/test', async(ctx) => {
-            ctx.body = 'test'
+        .get('/file', async(ctx) => {
+            ctx.body = 'Use post method for upload file,  input name is "file"'
         })
-        .post('/file', async(ctx) => {
-
+        .post('/file', upload.single('file'), async(ctx) => {
+            ctx.body = ctx.req.handleResult
         })
 }
